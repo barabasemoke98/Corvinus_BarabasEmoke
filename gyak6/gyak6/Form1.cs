904baecfc402dcf3b187acc44bf8eb49cbe5f865
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-10-18 12:13:27 +0200

    4

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,18 +13,20 @@ namespace gyak6
 {
     public partial class Form1 : Form
     {
-        var mnbService = new MNBArfolyamServiceSoapClient();
+        
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
-        var request = new GetExchangeRatesRequestBody()
-        {
-            currencyNames = "EUR",
-            startDate = "2020-01-01",
-            endDate = "2020-06-30"
-        };
-        var response = mnbService.GetExchangeRates(request);
-        var result = response.GetExchangeRatesResult;
+       
     }
 }
