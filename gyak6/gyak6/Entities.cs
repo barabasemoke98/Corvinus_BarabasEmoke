6054a964d26edd7e428c5e77429cd29df88b9174
Emőke Edit Barabás <barabas.emoke98@gmail.com>
2020-10-18 13:11:04 +0200

    6

---
+++ gyak6/gyak6/Entities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak6
+{
+    class Entities
+    {
+        public class RateData
+        {
+            public DateTime Date;
+            public string Currency { get; set; }
+            public decimal Value { get; set; }
+
+        }
+    }
+}
--- gyak6/gyak6/Form1.Designer.cs
+++ gyak6/gyak6/Form1.Designer.cs
@@ -29,12 +29,67 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chartRateData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.getExchangeRatesResponseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getExchangeRatesResponseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartRateData
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chartRateData.ChartAreas.Add(chartArea3);
+            this.chartRateData.DataSource = this.chartRateData.Images;
+            legend3.Name = "Legend1";
+            this.chartRateData.Legends.Add(legend3);
+            this.chartRateData.Location = new System.Drawing.Point(389, 41);
+            this.chartRateData.Name = "chartRateData";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chartRateData.Series.Add(series3);
+            this.chartRateData.Size = new System.Drawing.Size(542, 471);
+            this.chartRateData.TabIndex = 0;
+            this.chartRateData.Text = "chart1";
+            // 
+            // getExchangeRatesResponseBindingSource
+            // 
+            this.getExchangeRatesResponseBindingSource.DataSource = typeof(gyak6.MnbServiceReference.GetExchangeRatesResponse);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(329, 492);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(955, 573);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chartRateData);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chartRateData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.getExchangeRatesResponseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRateData;
+        private System.Windows.Forms.BindingSource getExchangeRatesResponseBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
